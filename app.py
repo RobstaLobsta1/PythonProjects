69148c80e9002bafcfd229706eba411ec30896ec
RobstaLobsta1 <rob@Robbys-MacBook-Pro.local>
2025-02-17 00:26:13 -0800

    Initial Commit

---
+++ app.py
@@ -0,0 +1,20 @@
+''' Recieve user inputs and store as variable
+name = input("What is your name? ")
+
+print("Hello, " + name + "!")
+'''
+
+'''Practice with built in functions (int and str)
+birth_year = input("Enter your birth year: ")
+age = 2024 - int(birth_year)
+print("You are " + str(age) + " years old.")
+'''
+
+'''Simple calculator taking user input'''
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+
+ans = int(num1) + int(num2)
+
+print("The answer is " + str(ans))
