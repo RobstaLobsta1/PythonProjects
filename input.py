d9636885ea7aa58b47df1686f0fc26d43632f75e
RobstaLobsta1 <rob@Robbys-MacBook-Pro.local>
2025-02-17 00:54:08 -0800

    Exercise 1 Complete

--- input.py
+++ input.py
@@ -1,6 +1,11 @@
-'''Grabbing user input and outputting after saving to var'''
+#Grabbing user input and outputting after saving to var
 name = input("Enter your name: ")
 
 age = input("Enter your age: ")
 
-print("Hello, " + name + "! You are " + age + " years old.")
+#Calculates what year the user will be 100 years old. 
+#Hard coded although and will need to be updated yOy
+modAge = 2024 + (100 -int(age))
+
+print("Hello, " + name + "! You are " + str(age) + " years old.")
+print("You are going to turn 100 in the year " + str(modAge))
