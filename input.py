16a9f5caaabb7d2b2f3e016eff60dc73714aa31a
RobstaLobsta1 <rob@Robbys-MacBook-Pro.local>
2025-02-17 01:06:53 -0800

    Compact Code

--- input.py
+++ input.py
@@ -1,11 +1,11 @@
 #Grabbing user input and outputting after saving to var
 name = input("Enter your name: ")
 
-age = input("Enter your age: ")
+age = int(input("Enter your age: "))
 
 #Calculates what year the user will be 100 years old. 
 #Hard coded although and will need to be updated yOy
-modAge = 2024 + (100 -int(age))
+modAge = 2024 + (100 - age)
 
 print("Hello, " + name + "! You are " + str(age) + " years old.")
 print("You are going to turn 100 in the year " + str(modAge))
