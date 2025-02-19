164fb1dc88e1629d06e2a38b96f2da1fcb744c01
RobstaLobsta1 <rskipp68@gmail.com>
2025-02-19 12:47:25 -0800

    Added multiple of 4 functionality

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Personal Programs I wrote to help me learn Python!
+
+All files are from https://www.practicepython.org/ 
+
--- modulus.py
+++ modulus.py
@@ -10,10 +10,24 @@ num1 = int(input("Enter a number : "))
 #1 is NOT equal to 0, the conditional statement resolves to odd or "else"
 
 #any number divisible by 2 is even
-if num1 % 2 == 0:
+if num1 % 4 == 0:
+    print("The number is a multiple of 4")
+elif num1 % 2 == 0:
     print("The number is even")
 else:
     print("The number is odd")
+
+    '''
+    For tomorrow ::: Add extras
+    
+    
+    1. If the number is a multiple of 4, print out a different message.
+
+    2. Ask the user for two numbers: one number to check (call it num) and 
+    one number to divide by (check). If check divides evenly into num, 
+    tell that to the user. If not, print a different appropriate message.
+    
+    '''
 
 
 
