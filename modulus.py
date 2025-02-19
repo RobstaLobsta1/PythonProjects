4081a740ce8787aebb7e9e292417bbce0e94647e
RobstaLobsta1 <rskipp68@gmail.com>
2025-02-19 12:58:03 -0800

    Loop Functionality

--- modulus.py
+++ modulus.py
@@ -1,21 +1,29 @@
 #Ask the user for a number. Depending on whether the number 
 #is even or odd, print out an appropriate message to the user.
 
-num1 = int(input("Enter a number : "))
+
 
 
 #Essentially the modulus operator "%" divdes the two inputs and spits out
 #the remainder of the operation. Example: 9 / 2 = 4 with a remainder of 1. 
 #The number 1 serves as the solution to the modulus operation, and since 
 #1 is NOT equal to 0, the conditional statement resolves to odd or "else"
+num1 = 0
 
-#any number divisible by 2 is even
-if num1 % 4 == 0:
-    print("The number is a multiple of 4")
-elif num1 % 2 == 0:
-    print("The number is even")
-else:
-    print("The number is odd")
+while num1 != "end":
+    num1 = input("Enter a number : ")
+
+    if int(num1) % 4 == 0:
+        print("The number is a multiple of 4")
+    elif int(num1) % 2 == 0:
+        print("The number is even")
+    else:
+        print("The number is odd")
+    
+
+
+
+    
 
     '''
     For tomorrow ::: Add extras
