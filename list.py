d9395a5fd00d83da4b8d2e86f4325abfadb93053
RobstaLobsta1 <rskipp68@gmail.com>
2025-02-19 23:04:12 -0800

    Extras Staging

--- list.py
+++ list.py
@@ -5,6 +5,19 @@
 '''
 Lists are appendable and you can iterate through them using loops. Elements can be either numerical or strings. 
 
+Exercise Extras
+
+
+    1. Instead of printing the elements one by one, make a new list 
+    that has all the elements less than 5 from this list in it and 
+    print out this new list.
+
+    2. Write this in one line of Python.
+
+    3. Ask the user for a number and return a list that contains only 
+    elements from the original list a that are smaller than that 
+    number given by the user.
+
 '''
 
 
