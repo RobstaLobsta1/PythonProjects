32ec6a92ee7c060f50624ed8a1e8b0157f25ee96
RobstaLobsta1 <rskipp68@gmail.com>
2025-02-19 22:59:32 -0800

    Exercise 3 Complete

--- list.py
+++ list.py
@@ -2,5 +2,8 @@
 
 #Write a program that prints out all the elements of the list that are less than 5
 
-a = [1,1,2,3,5,13,21,34,55,89]
+list = [1,1,2,3,5,13,21,34,55,89]
 
+for i in list:
+    if i < 5:
+        print(i)
