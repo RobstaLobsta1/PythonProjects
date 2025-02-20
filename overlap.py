173adf6ec1cd85358ca82083cd7f67c6218fa32f
RobstaLobsta1 <rskipp68@gmail.com>
2025-02-19 23:33:31 -0800

    Initial Commit

---
+++ overlap.py
@@ -0,0 +1,9 @@
+#Exercise 5
+
+'''
+Take two lists, and write a program that returns a list that 
+contains only the elements that are common between the lists 
+(without duplicates). Make sure your program works on two lists 
+of different sizes.
+'''
+
